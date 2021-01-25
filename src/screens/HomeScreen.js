fa17690c0e347e26be965e0b5455dcb8fa846ab1
Mark <marksmellie892@gmail.com>
2021-01-25 13:24:45 +0000

    Added buttons for Image and List screens

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,17 +1,22 @@
 import React from "react";
 import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = () => {
+const HomeScreen = ({ navigation }) => {
   return (
     <View>
       <Text style={styles.text}>Hi there!</Text>
       <Button 
-        onPress={() => console.log('Button pressed')}
+        onPress={() => navigation.navigate('Components')}
         title="Go to Components Demo" 
       />
-      <TouchableOpacity onPress={() => console.log('List Pressed')}>
-        <Text>Go to List Demo</Text>
-      </TouchableOpacity>
+      <Button 
+        onPress={() => navigation.navigate('List')} 
+        title="Go to List Demo"
+      />
+      <Button 
+        onPress={() => navigation.navigate('Image')} 
+        title="Go to Image Demo"
+      />
     </View>
   );
 };
